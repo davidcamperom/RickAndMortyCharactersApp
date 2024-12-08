3097c3a27277562bc1859ca478e00da946c4d3e5
davidcamperom <davidcampero26@gmail.com>
2024-12-08 22:59:59 +0100

    Initial Commit

---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by David Campero Maña on 8/12/24.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
